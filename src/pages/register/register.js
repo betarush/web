f0671c3ececb5682b45dd226991dba3a8d24166e
Kevin Mai <kmrobogram@gmail.com>
2023-09-06 13:35:44 -0400

    used material ui on register page

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -2,13 +2,44 @@ import './register.scss';
 import { useEffect, useState } from 'react';
 import { register } from '../../apis/user'
 
+// material ui components
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const defaultTheme = createTheme();
+
+const theme = createTheme({
+	palette: {
+		login: {
+			main: 'black',
+			contrastText: 'white'
+		}
+	}
+})
+
 export default function Register() {
 	const [email, setEmail] = useState('creator@gmail.com')
 	const [password, setPassword] = useState('qqqqqqq')
 	const [confirmPassword, setConfirmpassword] = useState('qqqqqqq')
 	const [errorMsg, setErrormsg] = useState('')
 
-	const theRegister = () => {
+	const theRegister = event => {
+		event.preventDefault();
+
+		const data = new FormData(event.currentTarget);
+		const email = data.get('email'), password = data.get('password'), confirmPassword = data.get('confirmPassword')
+
 		if (email && password && confirmPassword) {
 			if (email.includes("@")) {
 				if (password.length > 6 && password == confirmPassword) {
@@ -58,33 +89,42 @@ export default function Register() {
 	}
 
 	return (
-		<div id="register">
-			<div className="row">
-				<div id="page-navs">
-					<div className="column"><div className="page-nav" onClick={() => window.location = "/login"}>Login</div></div>
-				</div>
-			</div>
+		<ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'black' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">Register</Typography>
+          <Box component="form" onSubmit={theRegister} noValidate sx={{ mt: 1 }}>
+            <TextField margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus defaultValue={email}/>
+            <TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" defaultValue={password}/>
+            <TextField margin="normal" required fullWidth name="confirmPassword" label="Password" type="password" id="confirmPassword" autoComplete="current-password" defaultValue={confirmPassword}/>
 
-			<div id="form">
-				<div id="header">Register</div>
+            <Typography component="h1" variant="h6" color="red">{errorMsg}</Typography>
 
-				<div className="form-input">
-					<div className="input-header">Enter an e-mail:</div>
-					<input class="input" placeholder="Your e-mail" type="email" onChange={e => setEmail(e.target.value)} value={email}/>
-				</div>
-				<div className="form-input">
-					<div className="input-header">Enter a password:</div>
-					<input class="input" placeholder="Enter a password" type="password" onChange={e => setPassword(e.target.value)} value={password}/>
-				</div>
-				<div className="form-input">
-					<div className="input-header">Confirm your password:</div>
-					<input class="input" placeholder="Confirm password" type="password" onChange={e => setConfirmpassword(e.target.value)} value={confirmPassword}/>
-				</div>
-
-				<div id="errormsg">{errorMsg}</div>
-
-				<div id="submit" onClick={() => theRegister()}>Register</div>
-			</div>
-		</div>
+            <Button type="submit" fullWidth variant="contained" color="login" sx={{ mt: 3, mb: 2 }}>REGISTER</Button>
+            <Grid container>
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Log in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 8, mb: 4 }}>
+		      {'Copyright © ' + new Date().getFullYear() + ' Geottuse, Inc.'}
+		    </Typography>
+      </Container>
+    </ThemeProvider>
 	)
 }
